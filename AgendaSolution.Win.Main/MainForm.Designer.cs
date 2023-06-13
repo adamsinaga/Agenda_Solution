71a718c2566a919bcfb3f53b26dcad347e06cfd3
adamsinaga <38689336+adamsinaga@users.noreply.github.com>
2023-06-13 09:38:02 -0400

    Add files via upload
    Commit Win Main

---
+++ AgendaSolution.Win.Main/MainForm.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace AgendaSolution.Win.Main
+{
+    partial class AplicacaoAgendaMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            consultaToolStripMenuItem = new ToolStripMenuItem();
+            contatoToolStripMenuItem = new ToolStripMenuItem();
+            sobreToolStripMenuItem = new ToolStripMenuItem();
+            umaAplicaçãoDeAgendaParaDisciplinaDeOficinaDeSoftwareIToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { consultaToolStripMenuItem, sobreToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // consultaToolStripMenuItem
+            // 
+            consultaToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { contatoToolStripMenuItem });
+            consultaToolStripMenuItem.Name = "consultaToolStripMenuItem";
+            consultaToolStripMenuItem.Size = new Size(66, 20);
+            consultaToolStripMenuItem.Text = "Consulta";
+            // 
+            // contatoToolStripMenuItem
+            // 
+            contatoToolStripMenuItem.Name = "contatoToolStripMenuItem";
+            contatoToolStripMenuItem.Size = new Size(180, 22);
+            contatoToolStripMenuItem.Text = "Contato";
+            contatoToolStripMenuItem.Click += contatoToolStripMenuItem_Click;
+            // 
+            // sobreToolStripMenuItem
+            // 
+            sobreToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { umaAplicaçãoDeAgendaParaDisciplinaDeOficinaDeSoftwareIToolStripMenuItem });
+            sobreToolStripMenuItem.Name = "sobreToolStripMenuItem";
+            sobreToolStripMenuItem.Size = new Size(49, 20);
+            sobreToolStripMenuItem.Text = "Sobre";
+            // 
+            // umaAplicaçãoDeAgendaParaDisciplinaDeOficinaDeSoftwareIToolStripMenuItem
+            // 
+            umaAplicaçãoDeAgendaParaDisciplinaDeOficinaDeSoftwareIToolStripMenuItem.Name = "umaAplicaçãoDeAgendaParaDisciplinaDeOficinaDeSoftwareIToolStripMenuItem";
+            umaAplicaçãoDeAgendaParaDisciplinaDeOficinaDeSoftwareIToolStripMenuItem.Size = new Size(431, 22);
+            umaAplicaçãoDeAgendaParaDisciplinaDeOficinaDeSoftwareIToolStripMenuItem.Text = "Uma Aplicação de Agenda para disciplina de Oficina Dev. Software I";
+            // 
+            // AplicacaoAgendaMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "AplicacaoAgendaMain";
+            Text = "Aplicação de Agenda";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem consultaToolStripMenuItem;
+        private ToolStripMenuItem contatoToolStripMenuItem;
+        private ToolStripMenuItem sobreToolStripMenuItem;
+        private ToolStripMenuItem umaAplicaçãoDeAgendaParaDisciplinaDeOficinaDeSoftwareIToolStripMenuItem;
+    }
+}
